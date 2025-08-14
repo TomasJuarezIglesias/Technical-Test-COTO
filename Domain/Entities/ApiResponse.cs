230e4159855c7d508ea42510f2fb427b474f3886
Tomas Juarez <tjuarez.tm@hotmail.com>
2025-08-14 08:33:53 -0300

    Respuesta generica API + Cambio de naming Initializer controller

---
+++ Domain/Entities/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Domain.Entities
+{
+    public class ApiResponse<T>(bool success, T? data, string? message)
+    {
+        public bool Success { get; set; } = success;
+        public T? Data { get; set; } = data;
+        public string? Message { get; set; } = message;
+    }
+}
---
+++ Technical-Test-COTO/Controllers/DbInitializerController.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Technical_Test_COTO.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DbInitializerController(AppDbContext _dbContext, ILogger<DbInitializerController> _logger) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> InitializeDatabase()
+        {
+            await AppDbInitializer.MigrateAndSeedAsync(_dbContext, _logger);
+
+            var response = new ApiResponse<object>( success: true, data: null, message: "Base de datos inicializada correctamente");
+            return Ok(response);
+        }
+    }
+}
